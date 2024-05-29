59e99f1ef9e38f59da9c3c6f70668b97926eb98c
ig-rudenko <ig.rudenko1@yandex.ru>
2024-05-29 17:03:08 +0300

    v 0.2.9 Изменен api interceptor

--- frontend/src/services/setupInterceptors.ts
+++ frontend/src/services/setupInterceptors.ts
@@ -1,6 +1,71 @@
 import axiosInstance from "./api";
 import TokenService from "./token.service";
 import router from "@/router";
+import {AxiosError, AxiosResponse, InternalAxiosRequestConfig} from "axios";
+
+
+interface InterceptorInternalAxiosRequestConfig extends InternalAxiosRequestConfig {
+    _retry?: boolean;
+}
+
+
+async function logoutUser(store: any, resp: AxiosError<any>): Promise<AxiosResponse> {
+    store.dispatch("auth/logout")
+    await router.push("/login");
+    return Promise.reject(resp);
+}
+
+
+async function checkNotEnoughPermissions(store: any, resp: AxiosError<any>): Promise<undefined | AxiosResponse<any>> {
+    if (resp.response && resp.response.status === 403 && String(resp.response.data?.detail).includes("Not enough permissions")) {
+        return await logoutUser(store, resp)
+    }
+    return
+}
+
+
+async function checkRefreshTokens(store: any, resp: AxiosError<any>): Promise<undefined | AxiosResponse<any>> {
+    const originalConfig = <InterceptorInternalAxiosRequestConfig>resp.config!;
+
+    // Пропускаем URL, который отвечает за получение токена, чтобы не было рекурсии.
+    if (originalConfig.url == "/api/v1/auth/token") return;
+
+    // Пропускаем все коды ошибок кроме 401, а также повторные запросы.
+    if (!resp.response || resp.response.status !== 401 || originalConfig._retry) return;
+
+    // Access Token was expired
+    originalConfig._retry = true;  // Указываем, что это повторный запрос.
+    originalConfig.headers["Content-Type"] = "application/json";
+
+    try {
+        // Получаем refresh токен
+        const refreshToken = TokenService.getLocalRefreshToken()
+        if (!refreshToken) {
+            // Падаем в на страницу входа, если нет токена.
+            return await logoutUser(store, resp)
+        }
+
+        // Запрос на обновление токена.
+        const rs = await axiosInstance.post("auth/token/refresh", {refresh: refreshToken}, originalConfig)
+            .then(value => value, reason => reason.response)
+            .catch(reason => reason.response);
+
+        // Если произошла ошибка в обновлении access токена,
+        // то refresh токен неверный, надо залогиниться заново.
+        if (rs.status !== 200) {
+            return await logoutUser(store, resp)
+        }
+
+        const {access, refresh} = rs.data;
+        // Обновляем access и refresh токены.
+        store.dispatch('auth/refreshTokens', rs.data);
+        TokenService.updateLocalTokens(access, refresh);
+        return axiosInstance(originalConfig);
+
+    } catch (_error) {
+        return await logoutUser(store, resp)
+    }
+}
 
 const setup = (store: any) => {
     axiosInstance.interceptors.request.use(
@@ -11,60 +76,18 @@ const setup = (store: any) => {
             }
             return config;
         },
-        (error) => {
-            return Promise.reject(error);
-        }
+        error => Promise.reject(error)
     );
 
     axiosInstance.interceptors.response.use(
-        (res) => {
-            return res;
-        },
-        async (err) => {
-            const originalConfig = err.config;
+        res => res,
+        async (err: AxiosError<any>) => {
 
-            if ((originalConfig.url !== "/api/v1/auth/token" || originalConfig.url !== "/api/v1/auth/users") && err.response) {
-                // Access Token was expired
-                if (err.response.status === 401 && !originalConfig._retry) {
-                    originalConfig._retry = true;
-                    originalConfig.headers["Content-Type"] = "application/json";
+            let resp = await checkNotEnoughPermissions(store, err);
+            if (resp) return resp;
 
-                    try {
-                        const refreshToken = TokenService.getLocalRefreshToken()
-                        if (!refreshToken) {
-                            store.dispatch("auth/logout")
-                            await router.push("/login");
-                            return Promise.reject(err)
-                        }
-                        const rs = await axiosInstance.post(
-                            "auth/token/refresh",
-                            { refresh: refreshToken },
-                            originalConfig
-                        )
-                            .then(
-                                value => value,
-                                reason => reason.response
-                            )
-                            .catch(reason => reason.response);
-
-                        if (rs.status !== 200) {
-                            store.dispatch("auth/logout")
-                            await router.push("/login");
-                            return Promise.reject(err)
-                        }
-
-                        const { access, refresh } = rs.data;
-
-                        store.dispatch('auth/refreshTokens', rs.data);
-                        TokenService.updateLocalTokens(access, refresh);
-
-                        return axiosInstance(originalConfig);
-                    } catch (_error) {
-                        localStorage.removeItem("user")
-                        return Promise.reject(_error);
-                    }
-                }
-            }
+            resp = await checkRefreshTokens(store, err);
+            if (resp) return resp;
 
             return Promise.reject(err);
         }
